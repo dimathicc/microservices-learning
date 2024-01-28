1ca2fa35eba9e5ed2e19c23b4bcf08ec2137781d
Дмитрий Швалев <shvalevdd@mail.ru>
2024-01-29 00:16:42 +0500

    add JPA with H2 database and initial data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (10001, 'USD', 'RUB', 100, ''),
+       (10002, 'EUR', 'RUB', 110, ''),
+       (10003, 'AUD', 'RUB', 90, '');
