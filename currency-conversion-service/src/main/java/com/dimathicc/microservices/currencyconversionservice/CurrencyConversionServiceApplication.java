1469e1daf18a7beca508bc76a07145ca8aa8591d
Дмитрий Швалев <shvalevdd@mail.ru>
2024-01-29 14:45:02 +0500

    use rest template to connect currency-convertion and currency-exchange

--- currency-conversion-service/src/main/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,13 +1,13 @@
-package com.dimathicc.microservices.currencyconvertionservice;
+package com.dimathicc.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyConvertionServiceApplication {
+public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CurrencyConvertionServiceApplication.class, args);
+        SpringApplication.run(CurrencyConversionServiceApplication.class, args);
     }
 
 }
--- currency-conversion-service/src/test/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConvertionServiceApplicationTests.java
+++ currency-conversion-service/src/test/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConvertionServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dimathicc.microservices.currencyconvertionservice;
+package com.dimathicc.microservices.currencyconversionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
