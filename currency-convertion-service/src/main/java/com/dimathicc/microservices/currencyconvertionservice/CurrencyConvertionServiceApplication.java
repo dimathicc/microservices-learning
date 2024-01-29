fbe11c96e52ea7f37bc01488771c3172afbb7def
Дмитрий Швалев <shvalevdd@mail.ru>
2024-01-29 12:25:03 +0500

    add JPA to exchange-service and init convertion-service

---
+++ currency-convertion-service/src/main/java/com/dimathicc/microservices/currencyconvertionservice/CurrencyConvertionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dimathicc.microservices.currencyconvertionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConvertionServiceApplication.class, args);
+    }
+
+}
---
+++ currency-convertion-service/src/test/java/com/dimathicc/microservices/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimathicc.microservices.currencyconvertionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
