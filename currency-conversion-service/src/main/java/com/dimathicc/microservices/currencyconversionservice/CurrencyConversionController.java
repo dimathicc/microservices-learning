22170981eb5ba02b28970650dcdbb1e039306b85
Дмитрий Швалев <shvalevdd@mail.ru>
2024-01-30 12:41:57 +0500

    add Feign REST Client

--- currency-conversion-service/src/main/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/dimathicc/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -14,6 +14,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
+
+    private final CurrencyExchangeProxy proxy;
+    public CurrencyConversionController(CurrencyExchangeProxy proxy) {
+        this.proxy = proxy;
+    }
+
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
@@ -37,5 +43,22 @@ public class CurrencyConversionController {
                 quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment()+ " " + "rest template");
 
+    }
+
+
+
+    @GetMapping("/feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
     }
 }
