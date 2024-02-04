b67c72756048cb82595649243637f67cdbe281d4
Дмитрий Швалев <shvalevdd@mail.ru>
2024-02-04 18:43:58 +0300

    Add @Bulkhead to limit number of concurrent executions

--- currency-exchange-service/src/main/java/com/dimathicc/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/dimathicc/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.dimathicc.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +17,15 @@ public class CircuitBreakerController {
     private final Logger log = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    //@Retry(name = "default", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // @Retry(name = "default", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // @RateLimiter(name = "default") // 10 sec => 10000 calls to the sample api
+    @Bulkhead(name = "default") // only 10 concurrent calls allowed
     public String sampleApi() {
         log.info("Sample API call recieved");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception e) {
