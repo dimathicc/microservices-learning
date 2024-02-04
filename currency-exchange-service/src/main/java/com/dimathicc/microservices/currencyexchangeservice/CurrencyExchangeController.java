9488074d4024e66e528baee626e5a7d4357e5e4f
Дмитрий Швалев <shvalevdd@mail.ru>
2024-02-04 18:59:05 +0300

    connect Currency-exchange-service to zipkin

--- currency-exchange-service/src/main/java/com/dimathicc/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/dimathicc/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.dimathicc.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import java.math.BigDecimal;
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
 
+    private final Logger log = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     private final Environment environment;
     private final CurrencyExchangeRepository repository;
 
@@ -22,6 +26,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        log.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
